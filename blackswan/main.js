3693c2f181788dab31345d7c89f64fda0b4d8767
arixpsy <42609059+arixpsy@users.noreply.github.com>
2020-02-27 18:04:48 +0800

    3d

--- blackswan/main.js
+++ blackswan/main.js
@@ -1,33 +1,94 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+let scene, camera, renderer, cube, light;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.getElementById('background').appendChild( renderer.domElement );
+function init(){
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1 , 5 );
 
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshLambertMaterial( { color: 0xFFFFFF } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.getElementById('background').appendChild( renderer.domElement );
+    geometry = new THREE.IcosahedronGeometry();
+    material = new THREE.MeshLambertMaterial( { color: 0xFFFFFF } );
+    cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
 
-camera.position.z = 2;
+    camera.position.z = 2.5;
 
-var light = new THREE.PointLight( 0xFFFFFF, 1, 100);
-light.position.set( 0, 30, 0);
-scene.add( light );
+    light = new THREE.SpotLight( 0xFFFFFF, 1, 58);
+    light.position.set( 0, 20, 50);
+    scene.add( light );
 
-var light2 = new THREE.PointLight( 0xFFFFFF, 1, 100);
-light2.position.set( 0, -30, 0);
-scene.add( light2 );
+    scene.background = new THREE.Color(0x454A4D);
 
+}
 
-var animate = function () {
+function animate () {
     requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.001;
-    cube.rotation.y += 0.001;
+    cube.rotation.x += 0.005;
+    cube.rotation.y += 0.005;
 
     renderer.render( scene, camera);
 };
 
+init();
 animate();
+
+
+
+let currentSlide = 0;
+let lastSlide = document.querySelectorAll(".slide").length - 1;
+let scrolling = false;
+let slideTransitionTime = 1000;
+
+function nextSlide(){
+    if(currentSlide != lastSlide){
+        let slideDeck = document.querySelectorAll(".slide");
+        slideDeck[currentSlide].classList.add("slide-up");
+        slideDeck[currentSlide + 1].classList.remove("slide-down");
+        currentSlide++;
+    }
+}
+
+function previousSlide(){
+    if(currentSlide != 0){
+        let slideDeck = document.querySelectorAll(".slide");
+        slideDeck[currentSlide].classList.add("slide-down");
+        slideDeck[currentSlide - 1].classList.remove("slide-up");
+        currentSlide--;
+    }
+}
+function slideTransitionLock(){
+    setTimeout(()=>{
+        scrolling = false;
+    }, slideTransitionTime);
+}
+
+window.addEventListener('resize', ()=>{
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+});
+
+window.addEventListener('wheel', (event)=>{
+    let delta;
+    if (event.wheelDelta){
+        delta = event.wheelDelta;
+    }else{
+        delta = -1 *event.deltaY;
+    }
+
+    if (scrolling != true){
+        if (delta <= -50){
+            scrolling = true;
+            nextSlide();
+            slideTransitionLock();
+        }else if (delta >= 50){
+            scrolling = true;
+            previousSlide();
+            slideTransitionLock();
+        }
+        
+        
+    }
+});
